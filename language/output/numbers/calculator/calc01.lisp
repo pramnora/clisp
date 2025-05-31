f18feff5e7f2924171d237d30b6a2636b87682e2
Paul Ramnora <pramnora@yahoo.com>
2025-05-31 22:36:44 +0100

    Create calc01.lisp
    Created file: [calc01.lisp]. Demonstrates how to use CLisp to create a very simple calculator using functions: add()/subtract/multiply()/divide().

---
+++ language/output/numbers/calculator/calc01.lisp
@@ -0,0 +1,21 @@
+; ----------------------------
+; CREATED: 310525 14:36 PM GMT
+; UPDATED: 310525 14:36 PM GMT
+; ----------------------------
+
+; define functions
+
+(defun add (x y)(+ x y))
+(defun subtract (x y)(- x y))
+(defun multiply (x y)(* x y))
+(defun divide (x y)(/ x y))
+
+; call functions
+
+(print (add 1 2))      ; output: 3
+(print (subtract 1 2)) ; output: -1
+(print (multiply 1 2)) ; output: 2
+(print (divide 1 2))   ; output: 1/2
+
+
+
