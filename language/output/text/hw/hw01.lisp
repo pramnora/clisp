6157726ca0700f8d82d6a7d15a75211f1bd2a741
Paul Ramnora <pramnora@yahoo.com>
2025-05-31 23:02:34 +0100

    Update hw01.lisp
    Updated file: [hw01.lisp] to show 4 different version of: Hello, world!

--- language/output/text/hw/hw01.lisp
+++ language/output/text/hw/hw01.lisp
@@ -1,1 +1,6 @@
-(print "Hello, world!") ; output: Hello, world!
+(print "Hello, world!")      ; output: Hello, world!
+(write "Hello, world!")      ; output: Hello, world!
+(write-line "Hello, world!") ; output: Hello, world!
+(format t "Hello, world!")   ; output: Hello, world!
+
+            
